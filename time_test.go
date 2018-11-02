b38be5f6847ff550aa84c33a67c5e3e1680ad652
Yuki <paselan@gmail.com>
2018-11-02 12:41:25 +0900

    Add test cases

--- time_test.go
+++ time_test.go
@@ -1,6 +1,7 @@
 package vanatime_test
 
 import (
+	"fmt"
 	"testing"
 	"time"
 
@@ -34,5 +35,62 @@ func TestFromEarth(t *testing.T) {
 
 	if !got.Equal(want) {
 		t.Fatalf("want %v, but %v:", want, got)
+	}
+}
+
+func formatVanatime(vt vanatime.Time) string {
+	moon := vt.Moon()
+	return fmt.Sprintf(
+		"%s %s %s (%d%%)",
+		vt.Strftime("%Y-%m-%d %H:%M:%S %A"),
+		moon.Phase().StringLocale("ja"),
+		moon.Phase().String(),
+		moon.Percent(),
+	)
+}
+
+func TestEarthVariation(t *testing.T) {
+	patterns := [][2]string{
+		{"2018-11-01 00:00:00", "1312-12-11 00:00:00 Iceday 下弦の月 Last Quarter (57%)"},
+		{"2018-11-02 00:00:00", "1313-01-06 00:00:00 Lightningday 新月 New Moon (2%)"},
+		{"2018-11-03 00:00:00", "1313-02-01 00:00:00 Lightsday 十日夜 Waxing Gibbous (62%)"},
+	}
+
+	layout := "2006-01-02 15:04:05"
+	for i, pattern := range patterns {
+		et, err := time.ParseInLocation(layout, pattern[0], locJA)
+		if err != nil {
+			t.Fatalf("[%d]: %s", i, err)
+		}
+		vt := vanatime.FromEarth(et)
+		vs := formatVanatime(vt)
+
+		if vs != pattern[1] {
+			t.Errorf(`[%d]: want "%s", got "%s"`, i, pattern[1], vs)
+		}
+	}
+}
+
+func TestVanaVariation(t *testing.T) {
+	patterns := []struct {
+		V vanatime.Time
+		E string
+	}{
+		{vanatime.Date(1000, 3, 1, 0, 0, 0, 0), "2006-07-03 00:00:00.000000 Monday"},
+		{vanatime.Date(1000, 3, 2, 0, 0, 0, 0), "2006-07-03 00:57:36.000000 Monday"},
+		{vanatime.Date(1000, 3, 3, 0, 0, 0, 0), "2006-07-03 01:55:12.000000 Monday"},
+	}
+
+	layout := "2006-01-02 15:04:05.000000 Monday"
+	for i, pattern := range patterns {
+		et := pattern.V.Earth()
+		want, err := time.ParseInLocation(layout, pattern.E, locJA)
+		if err != nil {
+			t.Fatalf("[%d]: %s", i, err)
+		}
+
+		if !et.Equal(want) {
+			t.Errorf(`[%d]: want "%s", got "%s"`, i, want, et)
+		}
 	}
 }
