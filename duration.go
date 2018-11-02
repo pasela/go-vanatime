8da5de4939eaac4d6120d688a4f8f58cde2232ed
Yuki <paselan@gmail.com>
2018-11-02 22:10:19 +0900

    Add comments

--- duration.go
+++ duration.go
@@ -26,52 +26,10 @@ const (
 	Year                 = 360 * Day
 )
 
-func Since(t Time) Duration {
-	return Now().Sub(t)
-}
-
-func Until(t Time) Duration {
-	return t.Sub(Now())
-}
-
-func (d Duration) Microseconds() int64 {
-	return int64(d)
-}
-
-func (d Duration) Seconds() float64 {
-	sec := d / Second
-	usec := d % Second
-	return float64(sec) + float64(usec)/1e6
-}
-
-func (d Duration) Minutes() float64 {
-	min := d / Minute
-	usec := d % Minute
-	return float64(min) + float64(usec)/(60*1e6)
-}
-
-func (d Duration) Hours() float64 {
-	hour := d / Hour
-	usec := d % Hour
-	return float64(hour) + float64(usec)/(60*60*1e6)
-}
-
-func (d Duration) Truncate(m Duration) Duration {
-	if m <= 0 {
-		return d
-	}
-	return d - d%m
-}
-
-func (d Duration) Round(m Duration) Duration {
-	t := time.Duration(d * 1000).Round(time.Duration(m * 1000))
-	return Duration(t / 1000)
-}
-
-func (d Duration) String() string {
-	return time.Duration(d * 1000).String()
-}
-
+// ParseDuration parses a duration string. A duration string is a possibly
+// signed sequence of decimal numbers, each with optional fraction and a unit
+// suffix, such as "300ms", "-1.5h" or "2h45m". Valid time units are
+// "us" (or "µs"), "ms", "s", "m", "h".
 func ParseDuration(s string) (Duration, error) {
 	if strings.Contains(s, "ns") {
 		return 0, errors.New("vanatime: unknown unit ns in duration " + s)
@@ -82,4 +40,67 @@ func ParseDuration(s string) (Duration, error) {
 		return 0, err
 	}
 	return Duration(d / 1000), nil
+}
+
+// Microseconds returns the duration as an integer microsecond count.
+func (d Duration) Microseconds() int64 {
+	return int64(d)
+}
+
+// Seconds returns the duration as a floating point number of seconds.
+func (d Duration) Seconds() float64 {
+	sec := d / Second
+	usec := d % Second
+	return float64(sec) + float64(usec)/1e6
+}
+
+// Minutes returns the duration as a floating point number of minutes.
+func (d Duration) Minutes() float64 {
+	min := d / Minute
+	usec := d % Minute
+	return float64(min) + float64(usec)/(60*1e6)
+}
+
+// Hours returns the duration as a floating point number of hours.
+func (d Duration) Hours() float64 {
+	hour := d / Hour
+	usec := d % Hour
+	return float64(hour) + float64(usec)/(60*60*1e6)
+}
+
+// Truncate returns the result of rounding d toward zero to a multiple of m.
+// If m <= 0, Truncate returns d unchanged.
+func (d Duration) Truncate(m Duration) Duration {
+	if m <= 0 {
+		return d
+	}
+	return d - d%m
+}
+
+// Round returns the result of rounding d to the nearest multiple of m.
+// The rounding behavior for halfway values is to round away from zero.
+// If the result exceeds the maximum (or minimum) value that can be stored
+// in a Duration, Round returns the maximum (or minimum) duration.
+// If m <= 0, Round returns d unchanged.
+func (d Duration) Round(m Duration) Duration {
+	t := time.Duration(d * 1000).Round(time.Duration(m * 1000))
+	return Duration(t / 1000)
+}
+
+// String returns a string representing the duration in the form "72h3m0.5s".
+// Leading zero units are omitted. As a special case, durations less than one
+// second format use a smaller unit (milli-, microseconds) to ensure that the
+// leading digit is non-zero. The zero duration formats as 0s.
+func (d Duration) String() string {
+	return time.Duration(d * 1000).String()
+}
+
+// Since returns the time elapsed since t. It is shorthand for time.Now().Sub(t).
+func Since(t Time) Duration {
+	return Now().Sub(t)
+}
+
+// Until returns the duration until t. It is shorthand for t.Sub(time.Now()).
+func Until(t Time) Duration {
+	return t.Sub(Now())
 }
--- time.go
+++ time.go
@@ -57,10 +57,12 @@ type Time struct {
 	time int64
 }
 
+// Now returns the current Vana'diel time.
 func Now() Time {
 	return earth2vana(time.Now())
 }
 
+// Date returns the Time corresponding to given arguments.
 func Date(year, mon, day, hour, min, sec, usec int) Time {
 	sec, usec = norm(sec, usec, 1e6)
 	min, sec = norm(min, sec, 60)
@@ -80,36 +82,44 @@ func Date(year, mon, day, hour, min, sec, usec int) Time {
 	}
 }
 
+// FromEarth returns the Time corresponding to the given Earth time.
 func FromEarth(earth time.Time) Time {
 	return earth2vana(earth)
 }
 
+// FromInt64 returns the Time corresponding to the given Vana'diel time (since C.E. 0001-01-01 00:00:00).
 func FromInt64(time int64) Time {
 	return Time{
 		time: time,
 	}
 }
 
+// IsZero reports whether t represents the zero time instant.
 func (t Time) IsZero() bool {
 	return t.time == 0
 }
 
+// Before reports whether the time instant t is before u.
 func (t Time) Before(u Time) bool {
 	return t.time < u.time
 }
 
+// After reports whether the time instant t is after u.
 func (t Time) After(u Time) bool {
 	return t.time > u.time
 }
 
+// Equal reports whether t and u represent the same time instant.
 func (t Time) Equal(u Time) bool {
 	return t.time == u.time
 }
 
+// Add returns the time t+d.
 func (t Time) Add(d Duration) Time {
 	return Time{t.time + int64(d)}
 }
 
+// AddDate returns the time corresponding to adding the given number of years, months and days to t.
 func (t Time) AddDate(years int, months int, days int) Time {
 	year, month, day, _ := t.Date()
 	hour, min, sec := t.Clock()
@@ -120,6 +130,11 @@ func lessThanHalf(x, y Duration) bool {
 	return uint64(x)+uint64(x) < uint64(y)
 }
 
+// Truncate returns the result of rounding t down to a multiple of d.
+//
+// Truncate operates on the time as an absolute duration since the zero
+// time; it does not operate on the presentation form of the time. Thus,
+// Truncate(Hour) may return a time with a non-zero minute.
 func (t Time) Truncate(d Duration) Time {
 	if d <= 0 {
 		return t
@@ -128,6 +143,12 @@ func (t Time) Truncate(d Duration) Time {
 	return t.Add(-r)
 }
 
+// Round returns the result of rounding t to the nearest multiple of d.
+// The rounding behavior for halfway values is to round up.
+//
+// Round operates on the time as an absolute duration since the zero
+// time; it does not operate on the presentation form of the time. Thus,
+// Round(Hour) may return a time with a non-zero minute.
 func (t Time) Round(d Duration) Time {
 	if d <= 0 {
 		return t
@@ -139,6 +160,9 @@ func (t Time) Round(d Duration) Time {
 	return t.Add(d - r)
 }
 
+// Sub returns the duration t-u. If the result exceeds the maximum (or minimum)
+// value that can be stored in a Duration, the maximum (or minimum) duration
+// will be returned. To compute t-d for a duration d, use t.Add(-d).
 func (t Time) Sub(u Time) Duration {
 	d := Duration(t.time - u.time)
 	switch {
@@ -151,10 +175,12 @@ func (t Time) Sub(u Time) Duration {
 	}
 }
 
+// Earth returns the time of Earth.
 func (t Time) Earth() time.Time {
 	return vana2earth(t)
 }
 
+// Date returns the year, month, day and day of the year in which t occurs.
 func (t Time) Date() (year, mon, day, yday int) {
 	year = int(t.time/int64(Year)) + 1
 	mon = int(t.time%int64(Year)/int64(Month)) + 1
@@ -163,31 +189,37 @@ func (t Time) Date() (year, mon, day, yday int) {
 	return
 }
 
+// Year returns the year in which t occurs.
 func (t Time) Year() int {
 	year, _, _, _ := t.Date()
 	return year
 }
 
+// Month returns the month of the year specified by t.
 func (t Time) Month() int {
 	_, mon, _, _ := t.Date()
 	return mon
 }
 
+// Day returns the day of the month specified by t.
 func (t Time) Day() int {
 	_, _, day, _ := t.Date()
 	return day
 }
 
+// YearDay returns the day of the year specified by t, in the range [1,360].
 func (t Time) YearDay() int {
 	_, _, _, yday := t.Date()
 	return yday
 }
 
+// Weekday returns the day of the week specified by t.
 func (t Time) Weekday() Weekday {
 	wday := int(t.time % int64(Week) / int64(Day))
 	return Weekday(wday)
 }
 
+// Clock returns the hour, minute, and second within the day specified by t.
 func (t Time) Clock() (hour, min, sec int) {
 	hour = int(t.time % int64(Day) / int64(Hour))
 	min = int(t.time % int64(Hour) / int64(Minute))
@@ -195,29 +227,35 @@ func (t Time) Clock() (hour, min, sec int) {
 	return
 }
 
+// Hour returns the hour within the day specified by t, in the range [0, 23].
 func (t Time) Hour() int {
 	hour, _, _ := t.Clock()
 	return hour
 }
 
+// Minute returns the minute offset within the hour specified by t, in the range [0, 59].
 func (t Time) Minute() int {
 	_, min, _ := t.Clock()
 	return min
 }
 
+// Second returns the second offset within the minute specified by t, in the range [0, 59].
 func (t Time) Second() int {
 	_, _, sec := t.Clock()
 	return sec
 }
 
+// Microsecond returns the microsecond offset within the second specified by t, in the range [0, 999999].
 func (t Time) Microsecond() int {
 	return int(t.time % int64(Second))
 }
 
+// Int64 returns t as a int64 since C.E. 0001-01-01 00:00:00.
 func (t Time) Int64() int64 {
 	return t.time
 }
 
+// Moon returns the moon specified by t.
 func (t Time) Moon() Moon {
 	var days int = int(t.time / int64(Day))
 	timeOfMoon := (((int64(days) + 12) % 7) * int64(Day)) + (t.time % int64(Day))
@@ -228,6 +266,8 @@ func (t Time) Moon() Moon {
 	}
 }
 
+// String returns the time formatted using the format string.
+//	"%Y-%m-%d %H:%M:%S"
 func (t Time) String() string {
 	return t.Strftime("%Y-%m-%d %H:%M:%S")
 }
